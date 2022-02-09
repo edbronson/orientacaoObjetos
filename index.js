ad1209b54c5ce6357014b7494fe8cbeb51bdf6e7
Eder <ederbronson@gmail.com>
2022-02-09 20:19:16 -0300

    métodos com retorno

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ class cliente {
 class contaCorrente{
     agencia;
     //https://github.com/tc39/proposal-class-fields#private-fields
-    #saldo = 0;
+    _saldo = 0;
 
     sacar(valor){
-        if(this.#saldo >= valor){
-            this.#saldo -= valor;
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
         }
     }
 
     depositar(valor){
-        if(valor > 0){
-            this.#saldo += valor;
-            console.log(this.#saldo)
+        if(valor <= 0){
+            return;
         }
+
+        this._saldo += valor;
     }
 }
 
@@ -36,10 +38,13 @@ cliente2.cpf = 88822233309;
 const contaCorrenteRicardo = new contaCorrente();
 contaCorrenteRicardo.agencia = 1001;
 
-
-
 contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
 
 console.log(contaCorrenteRicardo);
 
