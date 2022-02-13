4af533c476b4f799727da6fa16dc19d46d665969
Eder <ederbronson@gmail.com>
2022-02-13 11:31:59 -0300

    usando o super constructor

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -6,4 +6,10 @@ export class ContaCorrente extends Conta{
         super(0, cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
     }
+
+    //sobrescrevendo o comportamente de sacar
+    sacar(valor){
+        let taxa = 1.1;
+       return this._sacar(valor, taxa);
+    }
 }
