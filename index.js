c0033caafa176f3b7f78fab0e7b3470bf5a32ce5
Eder <ederbronson@gmail.com>
2022-02-14 08:23:12 -0300

    interfaces

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ diretor.cadastrarSenha("123456")
 const gerente= new Gerente("Ricardo", 5000,  13234397867866)
 gerente.cadastrarSenha("123")
 
-const estaLogado = SistemaAutenticacao.login(diretor, "123456");
+const cliente = new Cliente ("Lais", 4775586466, "456");
 
-console.log(estaLogado)
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
